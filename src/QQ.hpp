d739079f3d917e6da93297aee0d99f6970480df5
pudelkoM <maximilian.pudelko@tum.de>
2017-06-07 15:31:46 +0200

    Supply std::align for g++ < 4.8

--- src/QQ.hpp
+++ src/QQ.hpp
@@ -23,6 +23,29 @@
        do { perror(__FILE__ ":" PP_STR(__LINE__) ":\n\t" msg); exit(EXIT_FAILURE); } while (0)
 
 
+#if __GNUC__ < 5
+/* Taken from LLVM libcxx - MIT Licence */
+namespace std {
+inline void*
+align(size_t alignment, size_t size, void*& ptr, size_t& space)
+{
+    void* r = nullptr;
+    if (size <= space)
+    {
+        char* p1 = static_cast<char*>(ptr);
+        char* p2 = reinterpret_cast<char*>(reinterpret_cast<size_t>(p1 + (alignment - 1)) & -alignment);
+        size_t d = static_cast<size_t>(p2 - p1);
+        if (d <= space - size)
+        {
+            r = p2;
+            ptr = r;
+            space -= d;
+        }
+    }
+    return r;
+}
+} // namespace std
+#endif
 
 namespace QQ {
     template<typename T>
