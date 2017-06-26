02f2aab7754cae8f4186a4a5b8b7b197521aa49c
pudelkoM <maximilian.pudelko@tum.de>
2017-06-26 21:01:34 +0200

    Remove unneeded code

--- flowscope.lua
+++ flowscope.lua
@@ -4,7 +4,6 @@ local stats  = require "stats"
 local pktLib = require "packet"
 local eth    = require "proto.ethernet"
 local ip     = require "proto.ip4"
-local ns     = require "namespaces"
 local log    = require "log"
 local pcap   = require "pcap"
 local pf     = require "pf"
@@ -28,8 +27,6 @@ function configure(parser)
 	local args = parser:parse()
 	return args
 end
-
-local trigger = ns.get()
 
 function master(args)
 	if not args.generate then
@@ -60,12 +57,8 @@ function master(args)
 		moon.startTask("continuousDumper", qq, i, args.path, pipes[i])
 	end
 	
--- 	local tracker = flowtracker.createHashmap(2^24, "map 0")
--- 	local tracker = flowtracker.createFlowtracker(2^24, "map 0")
 	local tracker = flowtracker.createTBBMapv4(2^20)
 	for i = 1, args.analyzeThreads do
--- 		moon.startTask("batched_rte_hash_Analyzer", qq, i, tracker, pipes)
--- 		moon.startTask("batched_flowtracker_Analyzer", qq, i, tracker, pipes)
 -- 		moon.startTask("dummyAnalyzer", qq, i)
 		moon.startTask("TBBAnalyzer", qq, i, tracker, pipes)
 	end
@@ -74,7 +67,6 @@ function master(args)
 		-- libmoon has no destroy function for pipes
 	end
 	
-	--moon.startSharedTask("signalTrigger")
 	moon.startSharedTask("fillLevelChecker", qq)
 	--moon.startTask("fillLevelChecker", qq)
 	moon.waitForTasks()
@@ -134,59 +126,11 @@ function traffic_generator(qq, id, packetSize, newFlowRate, rate)
 	txCtr:finalize()
 end
 
-local function handleTrigger(pkt)
-	trigger.lock(function()
-		if trigger.triggered then
-			-- only one concurrent dumper at the moment
-			log:warn("Found second trigger packet while dumper is running - concurrent dumpers are NYI.")
-			return
-		end
-		log:info("Found trigger packet, notifying dumper thread.")
-		if pkt then 
-			pkt:dump()
-			trigger.pkt = pkt:clone()
-			trigger.triggered = pkt:getTimestamp()
-		else -- not triggered by a packet but a signal
-			trigger.triggered = moon.getTime()
-		end
-	end)
-end
-
 function fillLevelChecker(qq)
 	while moon.running() do
 		print(green("[QQ] Stored buckets: ") .. qq:size() .. "/" .. qq:capacity() .. green(" Overflows: ") .. qq:getEnqueueOverflowCounter())
 		moon.sleepMillisIdle(1000)
 	end
-end
-
-local signallib = ffi.load("build/sigusr")
-ffi.cdef[[
-	void install_signal_handler();
-	bool check_signal();
-]]
-
--- I'm too stupid to use the syscall library with SIGUSR1, so this is done in C
-function signalTrigger()
-	signallib.install_signal_handler()
-	while moon.running() do
-		if signallib.check_signal() then
-			handleTrigger()
-		end
-		moon.sleepMillisIdle(1)
-	end
-end
-
-fooModule = {
-	rxCtr = stats:newManualRxCounter("FooModule #1", "plain"),
-	bar = function(pkt) end
-}
-
-function fooModule:analyze(pkt)
-	self.rxCtr:updateWithSize(1, pkt:getLength())
-end
-
-function fooModule:done()
-	self.rxCtr:finalize()
 end
 
 -- TODO
@@ -199,9 +143,6 @@ function TBBAnalyzer(qq, id, hashmap, pipes)
 	local hashmap = hashmap
 	local rxCtr = stats:newManualRxCounter("TBB Analyzer Thread #" .. id, "plain")
 	local epsilon = 2  -- allowed area around the avrg. TLL
-	
-	-- dummy filter for testing
--- 	filterPipe:send("src host 10.0.0.1")
 	
 	local tuple = ffi.new("struct ipv4_5tuple")
 	
@@ -225,67 +166,11 @@ function TBBAnalyzer(qq, id, hashmap, pipes)
 				for _, pipe in ipairs(pipes) do
 					pipe:send(event)
 				end
-			else
-				-- Flow normal
 			end
 		end
 		storage:release()
 	end
 	rxCtr:finalize()
-end
-
-function batched_rte_hash_Analyzer(qq, id, tracker, pipes)
-	local batchsize = 64
-	local addCtr = 0
-	local flowdata = ffi.new("struct foo_flow_data")
-	local tupleBatch = ffi.new("struct ipv4_5tuple[?]", batchsize)
-	local keyPtr = ffi.new("const void *[?]", batchsize)
-	for i = 0, batchsize do
-		keyPtr[i] = tupleBatch + i
-	end
-	print(keyPtr[3], tupleBatch + 3)
-	local positionsBatch = ffi.new("int32_t[?]", batchsize)
-	local rxCtr = stats:newManualRxCounter("QQ Analyzer Thread #" .. id, "plain")
-	while moon.running() do
-		local storage = qq:peek()
-		
-		for i = 0, storage:size() - 1 - batchsize, batchsize do
-			for j = 0, batchsize - 1 do
-				--print("Build phase", "i:", i, "j:", j)
-				local pkt = storage:getPacket(i + j)
-				rxCtr:updateWithSize(1, pkt.len)
-				local parsedPkt = pktLib.getUdp4Packet(pkt)
-				tupleBatch[j].ip_dst = parsedPkt.ip4:getDst()
-				tupleBatch[j].ip_src = parsedPkt.ip4:getSrc()
-				tupleBatch[j].port_dst = parsedPkt.udp:getDstPort()
-				tupleBatch[j].port_src = parsedPkt.udp:getSrcPort()
-				tupleBatch[j].proto = parsedPkt.ip4:getProtocol()
-			end
-			
-			--print("Performing lookup")
-			local r = tracker:lookupBatch(keyPtr, batchsize, positionsBatch)
-			--print("Lookup done")
-			if r < 0 then
-				print("Batch lookup failed")
-				break
-			end
-			
-			for i = 0, batchsize - 1 do
-				if positionsBatch[i] < 0 then
-					local r = tracker:add_key(tupleBatch[i])
-					if r < 0 then
-						print(id, "Add error:", r, "inserted items:", addCtr)
-						moon:stop()
-					end
-					addCtr = addCtr + 1
-				end
-			end
-		end
-		-- TODO handle rest
-		storage:release()
-	end
-	rxCtr:finalize()
-	print("[QQ Analyzer] Added flows:", addCtr)
 end
 
 function dummyAnalyzer(qq, id)
@@ -299,189 +184,6 @@ function dummyAnalyzer(qq, id)
 		storage:release()
 	end
 	rxCtr:finalize()
-end
-
-function batched_flowtracker_Analyzer(qq, id, tracker, pipes)
-	for i, pipe in ipairs(pipes) do
-		print("Analyzer", pipe)
-	end
-	
-	print("got", #pipes, "pipes")
-	local epsilon = 2
-	local batchsize = 64
-	local addCtr = 0
-	local flowdata = ffi.new("D") -- Reuse for every packet, the hash table copies it anyway
-	flowdata.rolling_sum = 0
-	flowdata.packet_counter = 1 -- Wrong, but atleast not a div by zero
-	local tupleBatch = ffi.new("struct ipv4_5tuple[?]", batchsize)
-	local keyPtr = ffi.new("const void *[?]", batchsize) -- needed for DPDK bulk lookup, cf. "bulk lookup explained.svg"
-	for i = 0, batchsize do
-		keyPtr[i] = tupleBatch + i
-	end
-	local dataPtrs = ffi.new("D* [?]", batchsize)
-	local positionsBatch = ffi.new("int32_t[?]", batchsize)
-	local rxCtr = stats:newManualRxCounter("QQ Analyzer Thread #" .. id, "plain")
-	while moon.running() do
-		local storage = qq:peek()
-		
-		-- TODO handle uneven batch sizes
-		for i = 0, storage:size() - 1 - batchsize, batchsize do
-			local TTLs = {}
-			for j = 0, batchsize - 1 do
-				local pkt = storage:getPacket(i + j)
-				rxCtr:updateWithSize(1, pkt.len)
-				local parsedPkt = pktLib.getUdp4Packet(pkt)
-				tupleBatch[j].ip_dst = parsedPkt.ip4:getDst()
-				tupleBatch[j].ip_src = parsedPkt.ip4:getSrc()
-				tupleBatch[j].port_dst = parsedPkt.udp:getDstPort()
-				tupleBatch[j].port_src = parsedPkt.udp:getSrcPort()
-				tupleBatch[j].proto = parsedPkt.ip4:getProtocol()
-				TTLs[j] = parsedPkt.ip4:getTTL()
-			end
-			
-			::try_again::
-			local r = tracker:lookupBatch4(keyPtr, batchsize, dataPtrs)
-			if r < 0 then
-				print("Batch lookup failed")
-				break
-			end
-			
-			for j = 0, batchsize - 1 do
-				if dataPtrs[j] == nil then
-					flowdata.rolling_sum = TTLs[j]  -- Set inital TTL
-					local r = tracker:addFlow4(tupleBatch[j], flowdata)
-					if r < 0 then
-						print(id, "Add error:", r, "inserted items:", addCtr)
-						moon:stop()
-					end
-					print(bred("[QQ Analyzer Thread #".. id .."]") .. ": New flow!", tupleBatch[j].ip_dst, tupleBatch[j].port_dst, tupleBatch[j].ip_src, tupleBatch[j].port_src)
-					addCtr = addCtr + 1
-					goto try_again -- This is crucial. If a new flow got added, the batch is worthless since it could contain multiple packets of the flow that just got added
-				else
-					local avrgTTL = dataPtrs[j].rolling_sum / dataPtrs[j].packet_counter
-					if (TTLs[j] > avrgTTL + epsilon or
-							TTLs[j] < avrgTTL - epsilon) then
-						local parsedPkt = pktLib.getUdp4Packet(storage:getPacket(i + j))
-						local event = {action = "create", filter = buildFilterExpr(parsedPkt)}
-						print(bred("[QQ Analyzer Thread #".. id .."]") .. ": Anomaly detected", dataPtrs[j].rolling_sum, dataPtrs[j].packet_counter, avrgTTL, TTLs[j], event.action, event.filter)
-						for _, pipe in ipairs(pipes) do
-							pipe:send(event)
-						end
-					end
-					-- FIXME: make atomic
-					dataPtrs[j].rolling_sum = dataPtrs[j].rolling_sum + TTLs[j]
-					dataPtrs[j].packet_counter = dataPtrs[j].packet_counter + 1
-				end
-			end
-		end
-		storage:release()
-	end
-	rxCtr:finalize()
-	print("[QQ Analyzer #" .. id .. "] Added flows:", addCtr)
-end
-
-function analyzer(qq, id, tracker)
-	local tracker = tracker or flowtracker.createHashmap(2^20, "map " .. tostring(id))
-	local flowdata = ffi.new("struct foo_flow_data")
-	local tuple = ffi.new("struct ipv4_5tuple")
-	local rxCtr = stats:newManualRxCounter("QQ Analyzer Thread #" .. id, "plain")
-	while moon.running() do
-		local storage = qq:peek()
-		for i = 0, storage:size() - 1 do
-			local pkt = storage:getPacket(i)
-			rxCtr:updateWithSize(1, pkt.len)
-			
-			local parsedPkt = pktLib.getUdp4Packet(pkt)
-			tuple.ip_dst = parsedPkt.ip4:getDst()
-			tuple.ip_src = parsedPkt.ip4:getSrc()
-			tuple.port_dst = parsedPkt.udp:getDstPort()
-			tuple.port_src = parsedPkt.udp:getSrcPort()
-			tuple.proto = parsedPkt.ip4:getProtocol()
-			
-			if tracker:lookup(tuple) < 0 then
-				local r = tracker:add_key(tuple)
-				if r < 0 then
-					print("Add error:", r)
-				end
-			end
-		end
-		storage:release()
-	end
-	rxCtr:finalize()
-	--tracker:delete()
-end
-
-local function filterDumperPacket(pkt)
-	local udpPkt = pktLib.getUdpPacket(pkt)
-	local triggerUdpPkt = pktLib.getUdpPacket(triggerPkt)
-	if udpPkt.ip4:getSrc() == triggerUdpPkt.ip4:getSrc() then
-		return true
-	end
-end
-
--- replace $foo in the dumper filter with values from the trigger packet
-local function fillExprFromPacket(expr, triggerPkt)
-	-- default values
-	local replacements = {
-		srcIP = "0.0.0.0", 
-		dstIP = "0.0.0.0",
-		srcPort = 0,
-		dstPort = 0,
-		proto = "udp"
-	}
-	if triggerPkt then -- triggerPkt will be nil if triggered by SIGUSR1
-		local pkt = pktLib.getEthPacket(triggerPkt)
-		if pkt.eth:getType() == eth.TYPE_IP then
-			-- actual L4 type doesn't matter
-			local pkt = pktLib.getUdp4Packet(triggerPkt)
-			replacements.srcIP = pkt.ip4:getSrcString()
-			replacements.dstIP = pkt.ip4:getDstString()
-			if pkt.ip4:getProtocol() == ip.PROTO_UDP then
-				replacements.srcPort = pkt.udp:getSrcPort()
-				replacements.dstPort = pkt.udp:getDstPort()
-				replacements.proto = "udp"
-			elseif pkt.ip4:getProtocol() == ip.PROTO_TCP then
-				-- port at the same position as UDP
-				replacements.srcPort = pkt.udp:getSrcPort()
-				replacements.dstPort = pkt.udp:getDstPort()
-				replacements.proto = "tcp"
-			elseif pkt.ip4:getProtocol() == ip.PROTO_SCTP then
-				-- port at the same position as UDP
-				replacements.srcPort = pkt.udp:getSrcPort()
-				replacements.dstPort = pkt.udp:getDstPort()
-				replacements.proto = "sctp"
-			else
-				replacements.proto = "ip"
-			end
-		elseif pkt.eth:getType() == eth.TYPE_IP6 then
-			local pkt = pktLib.getUdp6Packet(triggerPkt)
-			replacements.srcIP = pkt.ip6:getSrcString()
-			replacements.dstIP = pkt.ip6:getDstString()
-			if pkt.ip6:getNextHeader() == ip.PROTO_UDP then
-				replacements.srcPort = pkt.udp:getSrcPort()
-				replacements.dstPort = pkt.udp:getDstPort()
-				replacements.proto = "udp"
-			elseif pkt.ip6:getNextHeader() == ip.PROTO_TCP then
-				-- port at the same position as UDP
-				replacements.srcPort = pkt.udp:getSrcPort()
-				replacements.dstPort = pkt.udp:getDstPort()
-				replacements.proto = "tcp"
-			elseif pkt.ip6:getNextHeader() == ip.PROTO_SCTP then
-				-- port at the same position as UDP
-				replacements.srcPort = pkt.udp:getSrcPort()
-				replacements.dstPort = pkt.udp:getDstPort()
-				replacements.proto = "sctp"
-			else
-				replacements.proto = "ip"
-			end
-		end
-	end
-	return (expr:gsub("(%$%w*)", function(m) return replacements[m:sub(2)] end))
-end
-
-local function clearTrigger()
-	trigger.pkt = nil
-	trigger.triggered = nil
 end
 
 function continuousDumper(qq, id, path, filterPipe)
@@ -552,79 +254,6 @@ function continuousDumper(qq, id, path, filterPipe)
 		if rule.pcap then
 			rule.pcap:close()
 		end
-	end
-end
-
-function dumper(qq, path, dumpPast, dumpFuture, expr, code)
-	-- this loop currently supports only one running dumper
-	while moon.running() do
-		local triggered
-		local triggerPkt
-		while not triggered and moon.running() do
-			trigger.lock(function()
-				if trigger.triggered then
-					triggered = trigger.triggered
-					triggerPkt = trigger.pkt
-				end
-			end)
-			moon.sleepMillisIdle(10)
-		end
-		if not moon.running() then
-			return
-		end
-		local filter
-		if expr then
-			local substitutedExpr = fillExprFromPacket(expr, triggerPkt)
-			log:info("Using filter '%s' to dump pcap", substitutedExpr)
-			local pfFilter = pf.compile_filter(substitutedExpr)
-			filter = function(pkt)
-				return pfFilter(pkt.data, pkt.len)
-			end
-		else
-			filter = dofile(code)(triggerPkt)
-		end
-		-- we currently only sync once to systime at the trigger point
-		-- this clock will drift slightly for longer captures
-		-- (no, reasonable NUMA setups are not a problem, TSC is synced across CPUs since they share the same reference clock and reset)
-		local triggerWallTime = wallTime() - (moon.getTime() - triggered)
-		local pcapFileName = path .. "/FlowScope-dump-" .. os.date("%Y-%m-%d %H:%M:%S", triggerWallTime) .. ".pcap"
-		local pcapWriter = pcap:newWriter(pcapFileName, triggerWallTime - triggered)
-		log:info("Dumper starting to write %s", pcapFileName)
-		local stopDump = false
-		local progressShown = -math.huge
-		local dumpDuration
-		while moon.running() and not stopDump do
-			local storage = qq:tryDequeue()
-			if storage ~= nil then
-				local size = storage:size()
-				for i = 0, storage:size() - 1 do
-					local pkt = storage:getPacket(i)
-					local timestamp = pkt:getTimestamp()
-					local relativeTimestamp = timestamp - triggered
-					if relativeTimestamp > dumpFuture then
-						stopDump = true
-						break
-					end
-					if relativeTimestamp > -dumpPast then
-						if not dumpDuration then
-							dumpDuration = dumpFuture - relativeTimestamp
-							log:info("Dumper starting at %.1f seconds before trigger", -math.abs(relativeTimestamp))
-						end
-						if relativeTimestamp > progressShown + dumpDuration / 10 then
-							log:info("Dumper at %.1f/%.1f seconds", relativeTimestamp, dumpFuture)
-							progressShown = relativeTimestamp
-						end
-						if filter(pkt) then
-							pcapWriter:write(timestamp, pkt.data, pkt.len)
-						end
-					end
-				end
-				storage:release()
-			end
-		end
-		clearTrigger()
-		log:info("Flushing pcap file")
-		pcapWriter:close()
 	end
 end
 
--- flowtracker.lua
+++ flowtracker.lua
@@ -35,25 +35,6 @@ ffi.cdef [[
     uint32_t ipv4_5tuple_hash(struct ipv4_5tuple* tpl);
     uint32_t ipv6_5tuple_hash(struct ipv6_5tuple* tpl);
     
-    typedef struct ttl_flow_data D;
-
-    /* Flowtracker wrapper */
-    typedef struct flowtracker { } flowtracker_t;
-    flowtracker_t* flowtracker_create(uint32_t max_flows);
-    void flowtracker_delete(flowtracker_t* tr);
-    int32_t flowtracker_add_flow_v4(flowtracker_t*, const struct ipv4_5tuple* const, const D*);
-    D* flowtracker_get_flow_data_v4(flowtracker_t* tr, uint32_t ip_src, uint16_t port_src, uint32_t ip_dst, uint16_t port_dst, uint8_t proto);
-    int flowtracker_get_flow_data_bulk_v4(flowtracker_t* tr, const void* keys[], uint32_t num_keys, D* data[]);
-
-    /* rte_hash wrapper */
-    struct rte_hash { };
-    struct rte_hash* rte_hash_create_v4(uint32_t max_flows, const char* name);
-    void rte_hash_free(struct rte_hash *h);
-    int32_t rte_hash_add_key(const struct rte_hash *h, const void *key);
-    int32_t rte_hash_lookup(const struct rte_hash *h, const void *key);
-    int rte_hash_lookup_bulk(const struct rte_hash *h, const void **keys, uint32_t num_keys, int32_t *positions);
-    int32_t rte_hash_iterate(const struct rte_hash *h, const void **key, void **data, uint32_t *next);
-    
     /* TTL helpers */
     uint16_t get_average_TTL(uint64_t val);
     uint64_t update_TTL(uint64_t val, uint16_t new_ttl);
@@ -74,36 +55,6 @@ local C = ffi.C
 
 local mod = {}
 
-
-function mod.createFlowtracker(maxFlows)
-    return flowtrackerlib.flowtracker_create(maxFlows)
-end
-
-local flowtracker = {}
-flowtracker.__index = flowtracker
-local flowdata = {}
-flowdata.__index = flowdata
-
-
-function flowtracker:delete()
-    flowtrackerlib.flowtracker_delete(self)
-end
-
-function flowtracker:addFlow4(tuple, flow_data)
-    return flowtrackerlib.flowtracker_add_flow_v4(self, tuple, flow_data)
-end
-
-function flowtracker:get_flow_data_v4(ip_src, port_src, ip_dst, port_dst, proto)
-    return flowtrackerlib.flowtracker_get_flow_data_v4(self, ip_src, port_src, ip_dst, port_dst, proto)
-end
-
-function flowtracker:lookupBatch4(keys, numKeys, data)
-    return flowtrackerlib.flowtracker_get_flow_data_bulk_v4(self, keys, numKeys, data)
-end
-
-ffi.metatype("flowtracker_t", flowtracker)
-
-
 -- 5 tuple hash functions
 
 local ipv4_5tuple = {}
@@ -120,33 +71,6 @@ ffi.metatype("struct ipv6_5tuple", ipv6_5tuple)
 
 function ipv6_5tuple:hash()
     return flowtrackerlib.ipv6_5tuple_hash(self)
-end
-
-
--- rte_hash wrapper
-
-function mod.createHashmap(maxFlows, name)
-    return flowtrackerlib.rte_hash_create_v4(maxFlows, name)
-end
-
-local hash = {}
-hash.__index = hash
-ffi.metatype("struct rte_hash", hash)
-
-function hash:add_key(v4Tuple)
-    return C.rte_hash_add_key(self, v4Tuple)
-end
-
-function hash:lookup(v4Tuple)
-    return C.rte_hash_lookup(self, v4Tuple)
-end
-
-function hash:delete()
-    C.rte_hash_free(self)
-end
-
-function hash:lookupBatch(keys, numKeys, positions)
-    return C.rte_hash_lookup_bulk(self, keys, numKeys, positions)
 end
 
 
