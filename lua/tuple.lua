654e7872ecf59702b9871982b9de5b846d5276fb
pudelkoM <maximilian.pudelko@tum.de>
2017-09-10 17:35:39 +0200

    Make ip4 5-tuple printable

--- lua/tuple.lua
+++ lua/tuple.lua
@@ -1,4 +1,5 @@
 local ffi = require "ffi"
+local ip4 = require "proto.ip4"
 
 ffi.cdef [[
     struct ipv4_5tuple {
@@ -17,3 +18,23 @@ ffi.cdef [[
         uint8_t  proto;
     } __attribute__((__packed__));
 ]]
+
+local ip4Tuple = {}
+function ip4Tuple:__tostring()
+    local template = "ipv4_5tuple{ip_dst: %s, ip_src: %s, port_dst: %i, port_src: %i, proto: %s}"
+    local ip4AddrDst = ffi.new("union ip4_address")
+    local ip4AddrSrc = ffi.new("union ip4_address")
+    ip4AddrDst:set(self.ip_dst)
+    ip4AddrSrc:set(self.ip_src)
+    -- L4 Protocol
+    local proto = ""
+    if self.proto == ip4.PROTO_UDP then
+        proto = "udp"
+    elseif self.proto == ip4.PROTO_TCP then
+        proto = "tcp"
+    else
+        proto = tonumber(self.proto)
+    end
+    return template:format(ip4AddrDst:getString(), ip4AddrSrc:getString(), tonumber(self.port_dst), tonumber(self.port_src), proto)
+end
+ffi.metatype("struct ipv4_5tuple", ip4Tuple)
