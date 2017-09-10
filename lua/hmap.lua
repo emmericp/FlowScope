e3001c6e96e7e2b3dde8c99de3c8eabb7b3f883a
pudelkoM <maximilian.pudelko@tum.de>
2017-09-10 18:27:59 +0200

    Part of 0b90234f53803deed29a2f80fe71615449e66c07

--- lua/hmap.lua
+++ lua/hmap.lua
@@ -13,6 +13,7 @@ hmap{size}_accessor* hmap{size}_new_accessor();
 void hmap{size}_accessor_free(hmap{size}_accessor* a);
 void hmap{size}_accessor_release(hmap{size}_accessor* a);
 bool hmap{size}_access(hmap{size}* map, hmap{size}_accessor* a, const struct ipv4_5tuple* tpl);
+bool hmap{size}_erase(hmap{size}* map, hmap{size}_accessor* a);
 uint8_t* hmap{size}_accessor_get_value(hmap{size}_accessor* a);
 ]]
 
