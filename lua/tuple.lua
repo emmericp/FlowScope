e09f02fe53dd347092f8e293347bc48f5822ffd2
pudelkoM <maximilian.pudelko@tum.de>
2017-09-22 15:12:28 +0200

    Move common flow keys and extractor functions to library

--- lua/tuple.lua
+++ lua/tuple.lua
@@ -1,5 +1,10 @@
 local ffi = require "ffi"
+local pktLib = require "packet"
+local eth = require "proto.ethernet"
 local ip4 = require "proto.ip4"
+local ip6 = require "proto.ip6"
+
+local module = {}
 
 ffi.cdef [[
     struct ipv4_5tuple {
@@ -18,6 +23,11 @@ ffi.cdef [[
         uint8_t  proto;
     } __attribute__((__packed__));
 ]]
+
+module.flowKeys = {
+    "struct ipv4_5tuple",
+    "struct ipv6_5tuple",
+}
 
 local ip4Tuple = {}
 function ip4Tuple:__tostring()
@@ -38,3 +48,28 @@ function ip4Tuple:__tostring()
     return template:format(ip4AddrDst:getString(), ip4AddrSrc:getString(), tonumber(self.port_dst), tonumber(self.port_src), proto)
 end
 ffi.metatype("struct ipv4_5tuple", ip4Tuple)
+
+function module.extractIP5Tuple(buf, keyBuf)
+    local ethPkt = pktLib.getEthernetPacket(buf)
+    if ethPkt.eth:getType() == eth.TYPE_IP then
+        -- actual L4 type doesn't matter
+        keyBuf = ffi.cast("struct ipv4_5tuple&", keyBuf)
+        local parsedPkt = pktLib.getUdp4Packet(buf)
+        keyBuf.ip_dst = parsedPkt.ip4:getDst()
+        keyBuf.ip_src = parsedPkt.ip4:getSrc()
+        local TTL = parsedPkt.ip4:getTTL()
+        -- port is always at the same position as UDP
+        keyBuf.port_dst = parsedPkt.udp:getDstPort()
+        keyBuf.port_src = parsedPkt.udp:getSrcPort()
+        local proto = parsedPkt.ip4:getProtocol()
+        if proto == ip4.PROTO_UDP or proto == ip4.PROTO_TCP or proto == ip4.PROTO_SCTP then
+            keyBuf.proto = proto
+            return true, 1
+        end
+    elseif ethPkt.eth:getType() == eth.TYPE_IP6 then
+        -- FIXME: Add IPv6
+    end
+    return false
+end
+
+return module
