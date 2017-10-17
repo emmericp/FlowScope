e824f4bf9d20c9ce1905ed9597b5e061de29d97a
pudelkoM <maximilian.pudelko@tum.de>
2017-10-17 19:05:48 +0200

    Make default 5-Tuple flowkeys bidirectional

--- lua/tuple.lua
+++ lua/tuple.lua
@@ -8,10 +8,10 @@ local module = {}
 
 ffi.cdef [[
     struct ipv4_5tuple {
-        uint32_t ip_dst;
-        uint32_t ip_src;
-        uint16_t port_dst;
-        uint16_t port_src;
+        union ip4_address ip_a;
+        union ip4_address ip_b;
+        uint16_t port_a;
+        uint16_t port_b;
         uint8_t  proto;
     } __attribute__((__packed__));
 
@@ -24,6 +24,8 @@ ffi.cdef [[
     } __attribute__((__packed__));
 ]]
 
+assert(ffi.sizeof("struct ipv4_5tuple") == 13)
+
 module.flowKeys = {
     "struct ipv4_5tuple",
     "struct ipv6_5tuple",
@@ -31,13 +33,8 @@ module.flowKeys = {
 
 local ip4Tuple = {}
 
-local stringTemplate = "ipv4_5tuple{ip_dst: %s, ip_src: %s, port_dst: %i, port_src: %i, proto: %s}"
+local stringTemplate = "ipv4_5tuple{ip_a: %s, ip_b: %s, port_a: %i, port_b: %i, proto: %s}"
 function ip4Tuple:__tostring()
-    local ip4AddrDst = ffi.new("union ip4_address")
-    local ip4AddrSrc = ffi.new("union ip4_address")
-    ip4AddrDst:set(self.ip_dst)
-    ip4AddrSrc:set(self.ip_src)
-    -- L4 Protocol
     local proto
     if self.proto == ip4.PROTO_UDP then
         proto = "udp"
@@ -46,16 +43,12 @@ function ip4Tuple:__tostring()
     else
         proto = tostring(tonumber(self.proto))
     end
-    return stringTemplate:format(ip4AddrDst:getString(), ip4AddrSrc:getString(), tonumber(self.port_dst), tonumber(self.port_src), proto)
+    return stringTemplate:format(self.ip_a:getString(), self.ip_b:getString(), self.port_a, self.port_b, proto)
 end
 
 -- TODO: Rearrange expressions to generate better lua code in pflua
-local pflangTemplate = "src host %s src port %i dst host %s dst port %i %s"
-function ip4Tuple:getPflang()
-    local ip4AddrDst = ffi.new("union ip4_address")
-    local ip4AddrSrc = ffi.new("union ip4_address")
-    ip4AddrDst:set(self.ip_dst)
-    ip4AddrSrc:set(self.ip_src)
+local pflangTemplateUni = "src host %s src port %i dst host %s dst port %i %s"
+function ip4Tuple:getPflangUni()
     local proto
     if self.proto == ip4.PROTO_UDP then
         proto = "udp"
@@ -64,24 +57,30 @@ function ip4Tuple:getPflang()
     else
         proto = tostring(tonumber(self.proto))
     end
-    return pflangTemplate:format(ip4AddrSrc:getString(), tonumber(self.port_src), ip4AddrDst:getString(), tonumber(self.port_dst), proto)
+    return pflangTemplateUni:format(self.ip_a:getString(), self.port_a, self.ip_b:getString(), self.port_b, proto)
+end
+
+local pflangTemplate = "ip proto %i and host %s and host %s and port %i and port %i"
+function ip4Tuple:getPflang()
+    return pflangTemplate:format(self.proto, self.ip_a:getString(), self.ip_b:getString(), self.port_a, self.port_b)
 end
 
 ip4Tuple.__index = ip4Tuple
 ffi.metatype("struct ipv4_5tuple", ip4Tuple)
 
-function module.extractIP5Tuple(buf, keyBuf)
+-- Uni-directional
+function module.extractIP5TupleUni(buf, keyBuf)
     local ethPkt = pktLib.getEthernetPacket(buf)
     if ethPkt.eth:getType() == eth.TYPE_IP then
         -- actual L4 type doesn't matter
         keyBuf = ffi.cast("struct ipv4_5tuple&", keyBuf)
         local parsedPkt = pktLib.getUdp4Packet(buf)
-        keyBuf.ip_dst = parsedPkt.ip4:getDst()
-        keyBuf.ip_src = parsedPkt.ip4:getSrc()
-        local TTL = parsedPkt.ip4:getTTL()
+        -- IPs are copied in network byte order so that the getString() functions work
+        keyBuf.ip_a.uint32 = parsedPkt.ip4.src.uint32
+        keyBuf.ip_b.uint32 = parsedPkt.ip4.dst.uint32
         -- port is always at the same position as UDP
-        keyBuf.port_dst = parsedPkt.udp:getDstPort()
-        keyBuf.port_src = parsedPkt.udp:getSrcPort()
+        keyBuf.port_a = parsedPkt.udp:getSrcPort()
+        keyBuf.port_b = parsedPkt.udp:getDstPort()
         local proto = parsedPkt.ip4:getProtocol()
         if proto == ip4.PROTO_UDP or proto == ip4.PROTO_TCP or proto == ip4.PROTO_SCTP then
             keyBuf.proto = proto
@@ -89,6 +88,20 @@ function module.extractIP5Tuple(buf, keyBuf)
         end
     elseif ethPkt.eth:getType() == eth.TYPE_IP6 then
         -- FIXME: Add IPv6
+    end
+    return false
+end
+
+-- Bi-directional
+function module.extractIP5Tuple(buf, keyBuf)
+    local ok, idx = module.extractIP5TupleUni(buf, keyBuf)
+    if ok and idx == 1 then
+        keyBuf = ffi.cast("struct ipv4_5tuple&", keyBuf)
+        if keyBuf.ip_a.uint32 > keyBuf.ip_b.uint32 then
+            keyBuf.ip_a.uint32, keyBuf.ip_b.uint32 = keyBuf.ip_b.uint32, keyBuf.ip_a.uint32
+            keyBuf.port_a, keyBuf.port_b = keyBuf.port_b, keyBuf.port_a
+        end
+        return ok, idx
     end
     return false
 end
