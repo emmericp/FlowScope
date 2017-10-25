336665d634416729329ec88939b96f02802dc6db
pudelkoM <maximilian.pudelko@tum.de>
2017-10-25 12:48:41 +0200

    Track seen CIDs in separate hash map

--- lua/quicDetect.lua
+++ lua/quicDetect.lua
@@ -10,13 +10,14 @@ local hmap = require "hmap"
 local namespace = require "namespaces"
 local band = bit.band
 
+-- A protocol detector/tracker for quic as of RFC version 01
+-- https://tools.ietf.org/html/draft-ietf-quic-transport-01
+
 ffi.cdef [[
     int memcmp(const void *s1, const void *s2, size_t n);
 ]]
 
 local module = {}
-
--- A protocol detector/tracker for quic as of RFC version 00
 
 ffi.cdef [[
     struct conn_id {
@@ -33,8 +34,7 @@ ffi.cdef [[
         uint64_t last_seen;
         uint64_t first_seen;
         uint64_t connection_id;
-        uint16_t initial_port;
-        uint32_t initial_ip;
+        uint8_t cid_set;
         uint8_t tracked;
     };
 ]]
@@ -71,6 +71,8 @@ function module.extractFlowKey(buf, keyBuf)
     return false
 end
 
+log:setLevel("INFO")
+
 function module.handlePacket(flowKey, state, buf, isFirstPacket)
     local t = buf:getTimestamp() * 10^6
     state.last_seen = t
@@ -79,23 +81,26 @@ function module.handlePacket(flowKey, state, buf, isFirstPacket)
     end
 
     local udpPkt = pktLib.getUdp4Packet(buf)
-    --local quicFrame = udpPkt.payload.uint8
     local flags = udpPkt.payload.uint8[0]
+    if band(flags, 0x80) ~= 0 then
+        return false -- Reserved bit set, probably not quic
+    end
     if band(flags, 0x08) ~= 0 then
-        local dump = false
         local cid = ffi.cast("uint64_t*", udpPkt.payload.uint8 + 1)[0]
-        print("has CID!", cid)
+        log:debug("[Analyzer]: Found CID %s", tostring(cid))
         if not isFirstPacket and cid ~= state.connection_id then
-            log:warn("Connection ID changed for flow %s: %s -> %s", flowKey, tonumber(state.connection_id), tonumber(cid))
+            log:info("Connection ID changed for flow %s: %s -> %s", flowKey, tonumber(state.connection_id), tonumber(cid))
         end
         state.connection_id = cid
         -- Check ID -> 5-Tuple map
+        ffi.fill(IDkeyBuf, IDtable.keyBufSize())
         ffi.copy(IDkeyBuf, udpPkt.payload.uint8 + 1, 8)
         local new = IDtable:access(acc, IDkeyBuf)
         local tpl = ffi.cast("struct ipv4_5tuple&", acc:get())
         if new then
             ffi.copy(tpl, flowKey, ffi.sizeof("struct ipv4_5tuple"))
         end
+        local dump = false
         if ffi.C.memcmp(tpl, flowKey, ffi.sizeof("struct ipv4_5tuple")) ~= 0 then
             log:warn("Connection migration of id %i from %s to %s", tonumber(cid), tpl, flowKey)
             state.tracked = 1
@@ -104,9 +109,24 @@ function module.handlePacket(flowKey, state, buf, isFirstPacket)
         acc:release()
         return dump
     else
-        print("no CID!")
+        log:debug("[Analyzer]: No CID")
         if isFirstPacket then
-            log:warn("First packet in flow and no connection ID!")
+            log:info("First packet in flow and no connection ID in %s", flowKey)
+        else
+            ffi.fill(IDkeyBuf, IDtable.keyBufSize())
+            ffi.cast("uint64_t*", IDkeyBuf)[0] = state.connection_id
+            local exists = IDtable:find(acc, IDkeyBuf)
+            local dump = false
+            if exits then
+                local tpl = ffi.cast("struct ipv4_5tuple&", acc:get())
+                if ffi.C.memcmp(tpl, flowKey, ffi.sizeof("struct ipv4_5tuple")) ~= 0 then
+                    log:warn("Connection migration of id %i from %s to %s", tonumber(state.connection_id), tpl, flowKey)
+                    state.tracked = 1
+                    dump = true
+                end
+            end
+            acc:release()
+            return dump
         end
     end
     return false
@@ -120,6 +140,13 @@ module.checkInterval = 5
 function module.checkExpiry(flowKey, state, checkState)
     local t = lm.getTime() * 10^6
     if state.tracked == 1 and tonumber(state.last_seen) + 30 * 10^6 < t then
+        ffi.fill(IDkeyBuf, IDtable.keyBufSize())
+        ffi.cast("uint64_t*", IDkeyBuf)[0] = state.connection_id
+        --ffi.copy(IDkeyBuf, state.connection_id, 8)
+        local new = IDtable:access(acc, IDkeyBuf)
+        assert(new == false) -- Must hold or we have missed an ID before
+        IDtable:erase(acc)
+        acc:release()
         return true, tonumber(state.last_seen) / 10^6
     end
     return false
