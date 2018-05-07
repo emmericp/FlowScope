6aef16626e6d00a7db6fb2007bfa23e29b0affe3
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-05-07 16:52:38 +0200

    Rework example

--- examples/liveStatistician.lua
+++ examples/liveStatistician.lua
@@ -7,12 +7,17 @@ local tuple = require "tuple"
 
 local module = {}
 
+local inactiveFlowExpiry = 30 -- seconds
+
 ffi.cdef [[
     struct live_flow_state {
-        uint64_t packet_counter;
-        uint64_t byte_counter;
-        uint64_t first_seen;
+        uint64_t packets_interval;
+        uint64_t bytes_interval;
+        uint64_t packets_total;
+        uint64_t bytes_total;
         uint64_t last_seen;
+        uint64_t interval_start;
+        uint64_t first_seen;
     };
 ]]
 
@@ -23,52 +28,29 @@ module.extractFlowKey = tuple.extractIP5Tuple
 
 function module.handlePacket(flowKey, state, buf, isFirstPacket)
     local t = lm.getTime() * 10^6
-    state.packet_counter = state.packet_counter + 1
-    state.byte_counter = state.byte_counter + buf:getSize()
+    state.packets_interval = state.packets_interval + 1
+    state.bytes_interval = state.bytes_interval + buf:getSize()
+    state.packets_total = state.packets_total + 1
+    state.bytes_total = state.bytes_total + buf:getSize()
     if isFirstPacket then
         state.first_seen = t
+        state.interval_start = t
     end
     state.last_seen = t
 end
 
-
 module.checkInterval = 5
 
-ffi.cdef [[
-    struct check_state {
-        uint64_t start_time;
-        uint64_t active_flows;
-        uint64_t cumulative_packets;
-        uint64_t cumulative_bytes;
-    };
-]]
+-- ffi.cdef [[
+--     struct check_state {
+--         uint64_t start_time;
+--         uint64_t active_flows;
+--         uint64_t cumulative_packets;
+--         uint64_t cumulative_bytes;
+--     };
+-- ]]
 -- module.checkState = ffi.new("struct check_state")
 -- module.checkState = {}
-
-function module.checkExpiry(flowKey, flowState, checkState)
-    local t = lm.getTime() * 10^6
-
-    local b = flowState.byte_counter
-    local p = flowState.packet_counter
-    table.insert(checkState.tops, {b, p, flowKey})
-    table.sort(checkState.tops, function(a, b)
-        return a[1] > b[1]
-    end)
-    while #checkState.tops > 10 do
-        table.remove(checkState.tops)
-    end
-    -- Reset flow counter for most active flow in last 5 seconds, maybe use separate counter or expire faster
-    flowState.byte_counter = 0
-    flowState.packet_counter = 0
-    if flowState.last_seen + 30 * 10^6 < t then
-        return true, t / 10^6
-    else
-        checkState.active_flows = checkState.active_flows + 1
-        checkState.cumulative_packets = checkState.cumulative_packets + p
-        checkState.cumulative_bytes = checkState.cumulative_bytes + b
-        return false
-    end
-end
 
 function module.checkInitializer(checkState)
     checkState.start_time = lm.getTime() * 10^6
@@ -78,10 +60,37 @@ function module.checkInitializer(checkState)
     checkState.tops = {}
 end
 
+function module.checkExpiry(flowKey, flowState, checkState)
+    local t = lm.getTime() * 10^6
+
+    local b = flowState.bytes_interval
+    local p = flowState.packets_interval
+    table.insert(checkState.tops, {b, p, flowKey})
+    table.sort(checkState.tops, function(a, b)
+        return a[1] > b[1]
+    end)
+    while #checkState.tops > 10 do
+        table.remove(checkState.tops)
+    end
+    
+    -- Reset interval counter
+    flowState.bytes_interval = 0
+    flowState.packets_interval = 0
+
+    if flowState.last_seen + inactiveFlowExpiry * 10^6 < t then
+        return true, t / 10^6
+    else
+        checkState.active_flows = checkState.active_flows + 1
+        checkState.cumulative_packets = checkState.cumulative_packets + p
+        checkState.cumulative_bytes = checkState.cumulative_bytes + b
+        return false
+    end
+end
+
 function module.checkFinalizer(checkState)
     local t = lm.getTime() * 10^6
 
-    print("Top flows in this run [" .. module.checkInterval .. " s]:")
+    print("Top flows in this run [" .. module.checkInterval .. "s]:")
     print("#", "Bytes", "Packets", "Flow")
     for k,v in pairs(checkState.tops) do
         print(k, v[1], v[2], v[3])
